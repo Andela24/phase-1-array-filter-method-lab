929cf91444cb8d367eea6a6be4d95c995b3b9654
Andela Stevanovic <andjela.s166@gmail.com>
2021-12-14 07:54:51 -0500

    commit tree d2f030abe48fa2e495c1819fa50feeb55f57bf3d

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, string){
+    let result = drivers.filter(element => element.toLowerCase() === string.toLowerCase());
+    return result
+}
+
+
+function fuzzyMatch(drivers, string) {
+   let result = drivers.filter(word => word.startsWith(string))
+   return result
+} 
+
+function matchName(drivers, string){
+    let result = drivers.filter(element => element.name.match(string))
+ return result
+}
+
+
